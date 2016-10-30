343581b4b28cde65af7ab25f9e30fe3d23ea0d15
ykc <pica415@gmail.com>
2016-10-30 13:07:49 +0900

    속성 애니메이션

---
+++ Sunset/app/src/main/java/com/sunghyun/android/sunset/SunsetActivity.java
@@ -0,0 +1,12 @@
+package com.sunghyun.android.sunset;
+
+import android.support.v4.app.Fragment;
+
+public class SunsetActivity extends SingleFragmentActivity {
+
+
+    @Override
+    protected Fragment createFragment() {
+        return SunsetFragment.newInstance();
+    }
+}
